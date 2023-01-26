91af1cbc88f8de657c4adaaf8e247bcf31eaa876
JEMelendezSegura <esaums@gmail.com>
2023-01-25 23:18:39 -0600

    Avance hasta ejercicio de meses

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -167,6 +167,16 @@ function mesesDelAño(array) {
    // "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
    // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
    // Tu código:
+   var arrayNuevo = [];
+   for (let i = 0 ; i < array.length ; i++){
+      if (array [i] === "Enero" || array [i] === "Marzo" || array [i] === "Noviembre"){
+         arrayNuevo.push (array [i]);
+      }
+   }
+   if (arrayNuevo.length === 3){
+      return arrayNuevo;
+   }
+   return ("No se encontraron los meses pedidos");
 }
 
 function tablaDelSeis() {
